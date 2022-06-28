ec7e03607c1683450b1ffff8365283af7eafe44b
swj <159632_@naver.com>
2022-06-28 18:52:32 +0900

    Update : 블로그 내용 표시

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <p>인덱스 페이지</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        src="../images/clifford.jpg"
       />
     </Layout>
   );
