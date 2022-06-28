be7328928dc294962cf4468b78f301b5c030c18e
swj <159632_@naver.com>
2022-06-28 19:18:43 +0900

    Update : 포스트 클릭 시 링크 이동

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,10 @@ import Layout from "../components/layout";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>인덱스 페이지</p>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="../images/clifford.jpg"
+        src="../images/img.jpg"
       />
     </Layout>
   );
