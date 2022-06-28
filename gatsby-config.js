332f2e80403537d93921f0217bcd3b1f2ca4f19e
swj <159632_@naver.com>
2022-06-28 18:22:26 +0900

    Add : graphQL config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Dev Blog",
+    description: "Seong Wonjoon's Blog",
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
